04a91ec360ad58efd3d6107fef9b4e666881aec0
Anshita Bajpai <anshitapsit@gmail.com>
2026-07-01 13:08:21 +0530

    Add initial test class for ReverseEngineerApplication

---
+++ backend/src/test/java/com/reverseengineer/agent/ReverseEngineerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.reverseengineer.agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ReverseEngineerApplicationTests {
+
+    @Test
+    void contextLoads() {
+        
+    }
+}
